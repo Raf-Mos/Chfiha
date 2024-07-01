e9d4c1703514cb134a09e5e5038c9d7be46b5e1e
KamalALX <kamalmiftah01@gmail.com>
2024-07-01 21:43:42 +0100

    configuring static directory

--- chfiha/main/views.py
+++ chfiha/main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.views.generic import ListView
+from django.views.generic import TemplateView
+from django.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+class Home(TemplateView):
+    model = serc
+    template_name = 'home.html'
