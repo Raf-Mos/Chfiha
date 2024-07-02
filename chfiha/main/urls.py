44ce09c88cb9c6c37260996bce97b5c208d54688
KamalALX <kamalmiftah01@gmail.com>
2024-07-02 01:35:57 +0100

    updating views.py and urls.py for the app 'main'

--- chfiha/main/urls.py
+++ chfiha/main/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import Home
+from .views import AboutPageView, ContactPageView, HomePageView, LoginPageView, OrderPageView, RegisterPageView, ServicesPageView
+
 
 urlpatterns = [
-    path('', Home.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('', HomePageView.as_view(), name='home'),
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('order/', OrderPageView.as_view(), name='order'),
+    path('register/', RegisterPageView.as_view(), name='register'),
+    path('services/', ServicesPageView.as_view(), name='services'),
 ]
--- chfiha/main/views.py
+++ chfiha/main/views.py
@@ -3,29 +3,30 @@ from django.views.generic import TemplateView
 from .models import Project, Service
 
 
-class Home(ListView):
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+class HomePageView(ListView):
     model = Service
     template_name = 'home.html'
     context_object_name = 'all_services_list'
 
-class about(TemplateView):
-    template_name = 'about.html'
-
-class contact(TemplateView):
-    template_name = 'contact.html'
-
-class login(TemplateView):
+class LoginPageView(TemplateView):
     template_name = 'login.html'
 
-class order(ListView):
+class OrderPageView(ListView):
     model = Project
     template_name = 'order.html'
     context_object_name = 'all_projects_list'
 
-class register(TemplateView):
+class RegisterPageView(TemplateView):
     template_name = 'register.html'
 
-class services(ListView):
+class ServicePageView(ListView):
     model = Service
     template_name = 'services.html'
     context_object_name = 'all_services_list'
