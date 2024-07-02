461b3750f177243edea47e1ee0d5fa3f3cf8ceca
KamalALX <kamalmiftah01@gmail.com>
2024-07-02 01:18:25 +0100

    continue working on views module

--- chfiha/main/views.py
+++ chfiha/main/views.py
@@ -1,7 +1,31 @@
-# from django.views.generic import ListView
-from django.views.generic import TemplateView
-from django.
+from django.views.generic import ListView
+# from django.views.generic import TemplateView
+from .models import Project, Service
 
-class Home(TemplateView):
-    model = serc
+
+class Home(ListView):
+    model = Service
     template_name = 'home.html'
+    context_object_name = 'all_services_list'
+
+class about(TemplateView):
+    template_name = 'about.html'
+
+class contact(template_name):
+    template_name = 'contact.html'
+
+class login(template_name):
+    template_name = 'login.html'
+
+class order(ListView):
+    model = Project
+    template_name = 'order.html'
+    context_object_name = 'all_projects_list'
+
+class register(TemplateView):
+    template_name = 'register.html'
+
+class services(ListView):
+    model = Service
+    template_name = 'services.html'
+    context_object_name = 'all_services_list'
