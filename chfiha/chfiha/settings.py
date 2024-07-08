b53879748ee6b8a3238bc3ea08675248a44586a6
KamalALX <kamalmiftah01@gmail.com>
2024-07-09 00:23:50 +0100

    updating settings

--- chfiha/chfiha/settings.py
+++ chfiha/chfiha/settings.py
@@ -146,4 +146,7 @@ COMPRESS_ROOT = BASE_DIR / 'static'
 
 COMPRESS_ENABLED = True
 
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',)
