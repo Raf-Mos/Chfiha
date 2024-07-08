9b6be07413050d23d4a8dd6a7935563df3de9168
KamalALX <kamalmiftah01@gmail.com>
2024-07-08 16:59:29 +0100

    add

--- chfiha/accounts/forms.py
+++ chfiha/accounts/forms.py
@@ -2,25 +2,35 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
-
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(
-            widget=forms.TextInput(attrs={'placeholder': 'Email'})
-            )
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Email',
+            'class': 'mt-1 px-4 py-2 block w-full border border-gray-300 rounded-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
+        })
+    )
     first_name = forms.CharField(
-            max_length=30, widget=forms.TextInput(
-                attrs={'placeholder': 'First Name'})
-            )
+        max_length=30, widget=forms.TextInput(attrs={
+            'placeholder': 'First Name',
+            'class': 'mt-1 px-4 py-2 block w-full border border-gray-300 rounded-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
+        })
+    )
     last_name = forms.CharField(
-            max_length=30, widget=forms.TextInput(
-                attrs={'placeholder': 'Last Name'})
-            )
+        max_length=30, widget=forms.TextInput(attrs={
+            'placeholder': 'Last Name',
+            'class': 'mt-1 px-4 py-2 block w-full border border-gray-300 rounded-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
+        })
+    )
 
     # Override the labels for password fields
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['password1'].widget.attrs.update({'placeholder': 'New Password'})
-        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm New Password'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'New Password',
+                                                      'class': 'mt-1 px-4 py-2 block w-full border border-gray-300 rounded-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
+                                                      })
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm New Password',
+                                                      'class': 'mt-1 px-4 py-2 block w-full border border-gray-300 rounded-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
+                                                      })
 
     class Meta:
         model = CustomUser
@@ -31,3 +41,4 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
+
